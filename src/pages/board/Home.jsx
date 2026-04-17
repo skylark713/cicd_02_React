93bdcc94b8fa1008448408c14a90ae08a7018778
Yejin Lee <wthb22923@gmail.com>
2026-04-17 16:12:41 +0900

    CI/CD 시작하기

--- src/pages/board/Home.jsx
+++ src/pages/board/Home.jsx
@@ -1,42 +1,38 @@
 import { useContext, useEffect, useState } from "react";
 import BoardItem from "../../components/BoardItem";
 import "./Home.css";
-import axios from 'axios';
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { LoginContext } from "../../App";
 
-
-
-
-const Home =()=>{
+const Home = () => {
   const serverIp = import.meta.env.VITE_API_SERVER_IP;
- //console.log("serverIp = "+serverIp)
+  //console.log("serverIp = "+serverIp)
 
-  const { handleLoginChange} = useContext(LoginContext);
+  const { handleLoginChange } = useContext(LoginContext);
 
-  
+  //DB 목록을 저장해서 관리 할 useState
+  const [boards, setBoards] = useState([]);
 
-   //DB 목록을 저장해서 관리 할 useState
-   const [boards , setBoards] = useState([]);
+  const nav = useNavigate();
 
-   const nav = useNavigate();
-
-     //서버에서 데이터 조회   - axios
-     useEffect(()=>{
-        axios
-        //.get("http://3.34.71.73:9000/boards", {
-          .get(serverIp+"/boards", {
+  //서버에서 데이터 조회   - axios
+  useEffect(() => {
+    axios
+      //.get("http://3.34.71.73:9000/boards", {
+      .get(serverIp + "/boards", {
         headers: {
           Authorization: localStorage.getItem("Authorization"),
-        },})
-        .then((res)=>{
-           // console.log(res);
+        },
+      })
+      .then((res) => {
+        // console.log(res);
 
-           setBoards(res.data);//state에서 관리되는 변수가 변경되어 update(re-rendering)
-        })
-        .catch((err)=>{
-            console.log(err)
-            /*if(err.response.status===403){
+        setBoards(res.data); //state에서 관리되는 변수가 변경되어 update(re-rendering)
+      })
+      .catch((err) => {
+        console.log(err);
+        /*if(err.response.status===403){
                localStorage.clear();//모든 세션의 정보 지우기
 
                handleLoginChange(false);
@@ -45,20 +41,17 @@ const Home =()=>{
             }else{
                 alert(err.response.data.detail) ; 
             }*/
-               
-       }); 
-     }, []);
+      });
+  }, []);
 
+  return (
+    <div className="Home">
+      <h1>전체게시물 LIST - Yejin</h1>
+      {boards.map((board) => (
+        <BoardItem key={board.id} board={board} />
+      ))}
+    </div>
+  );
+};
 
-    return (
-      <div className="Home">
-       <h1>전체게시물 LIST</h1>
-        {
-          boards.map( (board)=> <BoardItem key={board.id} board={board}/> )
-        }   
-
-      </div>
-    )
-  }
-  
-  export default Home;
+export default Home;
